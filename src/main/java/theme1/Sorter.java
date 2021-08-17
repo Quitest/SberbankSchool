f28a22bb08929caaf8ecb7733e2427720c03ec52
Quitest <Quitest>
2021-08-17 23:52:14 +0300

    Initial commit

---
+++ src/main/java/theme1/Sorter.java
@@ -0,0 +1,33 @@
+package theme1;
+
+import java.util.Arrays;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class Sorter {
+    
+    public static void main(String[] args) {
+        Sorter sorter = new Sorter();
+        int[] array = IntStream.generate(()->(int)(Math.random() * 10))
+                .limit(5)
+                .toArray();
+        System.out.println(Arrays.toString(array));
+
+        sorter.bubleSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public int[] bubleSort(int[] array){
+        for (int i=0;i< array.length;i++){
+            for (int j= array.length-1; j>i; j--){
+                if (array[j-1] > array[j]){
+                    int tmp = array[j-1];
+                    array[j-1]=array[j];
+                    array[j]=tmp;
+                }
+            }
+        }
+        return array;
+    }
+}
