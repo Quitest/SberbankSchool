0cdd93e3c4afed8041cf7e54045787d927c407f2
Quitest <Quitest>
2021-08-18 21:38:14 +0300

    Финальная версия.

--- src/main/java/theme1/Sorter.java
+++ src/main/java/theme1/Sorter.java
@@ -1,33 +1,65 @@
 package theme1;
 
 import java.util.Arrays;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Sorter {
+    public static final int MIN_GEN_VAL = -10;
+    public static final int MAX_GEN_VAL = 10;
+    public static final int GENERATION_LIMIT = 10;
 
     public static void main(String[] args) {
-        Sorter sorter = new Sorter();
-        int[] array = IntStream.generate(()->(int)(Math.random() * 10))
-                .limit(10)
-                .toArray();
-        System.out.println(Arrays.toString(array));
+        System.out.println("Пример Integer.");
+        Integer[] arrayInteger = Stream
+                .generate(() -> (int) (Math.random() * (MAX_GEN_VAL - MIN_GEN_VAL) + MIN_GEN_VAL))
+                .limit(GENERATION_LIMIT)
+                .toArray(Integer[]::new);
+        System.out.println("Исходный: " + Arrays.toString(arrayInteger));
+        Sorter.bubleSortGeneric(arrayInteger);
+        System.out.println("Отсортированный: " + Arrays.toString(arrayInteger));
 
-        sorter.bubleSort(array);
-
-        System.out.println(Arrays.toString(array));
+        System.out.println("\nПример Byte.");
+        Byte[] arrayByte = Stream.generate(() -> (byte) (Math.random() * (MAX_GEN_VAL - MIN_GEN_VAL) + MIN_GEN_VAL))
+                .limit(GENERATION_LIMIT)
+                .toArray(Byte[]::new);
+        System.out.println("Исходный: " + Arrays.toString(arrayByte));
+        Sorter.bubleSortGeneric(arrayByte);
+        System.out.println("Отсортированный: " + Arrays.toString(arrayByte));
     }
-    //TODO массив - ссылочный тип. Проверить изменится ли состояние массива после вызова метода
-    public int[] bubleSort(int[] array){
-        for (int i=0;i< array.length;i++){
-            for (int j= array.length-1; j>i; j--){
-                if (array[j-1] > array[j]){
-                    int tmp = array[j-1];
-                    array[j-1]=array[j];
-                    array[j]=tmp;
+
+    /**
+     * Попытка реализации обобщенного метода сортировки пузырьком.
+     *
+     * @param array сортируемый массив
+     * @param <T>   наследник java.lang.Number, реализующий java.lang.Comparable
+     */
+    public static <T extends Number & Comparable<T>> void bubleSortGeneric(T[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1].compareTo(array[j]) > 0) {
+                    T tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
                 }
             }
         }
-        return array;
+    }
+
+    /**
+     * Метод сортировки целочисленного массива пузырьком.
+     * @param array соритруемый массив
+     */
+    public static void bubleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    int tmp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
     }
 }
+
+
