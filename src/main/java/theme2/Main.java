bbe2e33437ed92f02ae63f71d1d44c90a478f471
Quitest <Quitest>
2021-08-22 22:07:33 +0300

    Выполнены все задания темы 2, кроме №5 (Iterator).

--- src/main/java/theme2/Main.java
+++ src/main/java/theme2/Main.java
@@ -1,13 +1,16 @@
 package theme2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.*;
+
 import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
+    private static final int LIMIT = 20; //устанавливает ограничение на количество элементов, выдаваемых в потоках.
+
     public static void main(String[] args) {
-        System.out.println("\tЗадача 1. Разбить исходный список на группы по типу авто");
+        System.out.println("Задача 1. Разбить исходный список на группы по типу авто.");
         //Создаем исходный список авто
         List<Car> carList = new ArrayList<Car>() {{
             add(new Car("Лада", "седан"));
@@ -28,9 +31,57 @@ public class Main {
 
         carsGroupedByType.forEach(
                 (type, cars) -> {
-                    System.out.println("Доступные модели авто типа " + type + ":");
+                    System.out.println("\tДоступные модели авто типа " + type + ":");
                     //Т.к. cars это List, то используем forEach повторно для вывода всех элементов List<Car>
-                    cars.forEach(car -> System.out.println("\t" + car.getModel()));
+                    cars.forEach(car -> System.out.println("\t\t" + car.getModel()));
                 });
+        //--------------------------------------------------------------------------------------------------------------
+        System.out.println("Задача 2. Анализ текста.");
+        TextAnalizer analizer = new TextAnalizer(Paths.get("story.txt"), StandardCharsets.UTF_8);
+        Map<String, Long> words = analizer.analize();
+
+        System.out.println("2.1. Количество различных слов в файле: " + words.keySet().size());
+
+        System.out.println("2.2. Список различных слов файла, отсортированный по возрастанию их длины (компаратор сначала " +
+                "по длине слова, потом по тексту) [первые " + LIMIT + " слов]:");
+        words.keySet()
+                .stream()
+                .sorted(Comparator.comparingLong(String::length).thenComparing(Comparator.naturalOrder()))
+                .limit(LIMIT)
+                .forEach(w -> System.out.println("\t" + w));
+        System.out.println("\t...");
+
+        System.out.println("2.3. Подсчитать и вывести на экран сколько раз каждое слово встречается в файле " +
+                "[первые " + LIMIT + " слов]:");
+        System.out.printf("\t%-12s | %s %n", "<Слово>", "<Количество>");
+        words.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .limit(LIMIT)
+                .forEach(entry -> System.out.printf("\t%-12s | %2d%n", entry.getKey(), entry.getValue()));
+        System.out.println("\t...");
+
+        System.out.println("2.4. Выведите на экран все строки файла в обратном порядке.");
+        List<String> reversedText = analizer.getText();
+        Collections.reverse(reversedText);
+        reversedText.stream()
+                .limit(LIMIT)
+                .forEachOrdered(x -> System.out.println("\t" + x));
+
+        //Задание 5: Реализуйте свой Iterator для обхода списка в обратном порядке.
+        System.out.println("2.5. Реализуйте свой Iterator для обхода списка в обратном порядке.");
+
+
+        System.out.println("2.6. Вывести на экран строки, номера которых задаются пользователем в произвольном порядке.");
+        //Вариант 1. Простой вывод
+        List<String> stringList = analizer.getStrings(0, 1, 2, 10, 3, 5, 3);
+        stringList.forEach(line -> System.out.println("\t" + line));
+
+        //Вариант 2. Наглядный вывод
+//        int[] lineNumbers = {0,1,2,10,3,5,3};
+//        List<String> stringList = analizer.getStrings(lineNumbers);
+//        for (int i=0; i<stringList.size();i++){
+//            System.out.printf("\t[%d] %s %n", lineNumbers[i]+1, stringList.get(i));
+//        }
+
     }
 }
---
+++ src/main/java/theme2/TextAnalizer.java
@@ -0,0 +1,79 @@
+package theme2;
+
+import lombok.Getter;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Класс предназначен для анализа относительно небольших текстов согласно заданию.
+ */
+public class TextAnalizer {
+    private final Charset charset;
+    private final Path pathToFile;
+    @Getter
+    private List<String> text;
+
+    public TextAnalizer(Path pathToFile, Charset charset) {
+        this.charset = charset;
+        this.pathToFile = pathToFile;
+        try {
+            text = Files.readAllLines(pathToFile, charset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод позволяет выполнить анализ текста согласно заданию:
+     * <ul>
+     * <li>подсчитать количество различных слов в файле - количество записей в Map;</li>
+     * <li>подсчитать сколько раз каждое слово встречается в файле - значение value.</li>
+     * </ul>
+     *
+     * @return {@code Map<String, Long>}, где:
+     * <ul>
+     * <li>key - слово, которое встречается в тексте;</li>
+     * <li>value - сколько раз слово встречается в тексте.</li>
+     * </ul>
+     */
+    public Map<String, Long> analize() {
+        Map<String, Long> words = new HashMap<>();
+        try (Stream<String> lines = Files.lines(pathToFile, charset)) {
+            words = lines
+                    .flatMap(line -> Arrays.stream(line.split("[^а-яА-ЯёЁ0-9]")))
+                    .filter(e -> !e.isEmpty())
+                    .map(word -> word.toLowerCase(Locale.ROOT))
+                    .collect(Collectors.groupingBy(
+                            Function.identity(),
+                            Collectors.counting()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+
+    /**
+     * Возвращает строки, номера которых задаются пользователем в произвольном порядке. Нумерация строк начинается с 0.
+     * Если не указано ниодной строки, то возвращается {@code List<String>} с нулевым размером
+     * Строки возвращаются в том порядке, в котором они и были указаны - не сортируются.
+     *
+     * @param numbers номера строк, которые необходимо получить
+     * @return список запрошенных строк.
+     */
+    public List<String> getStrings(int... numbers) {
+        List<String> requestedLines = new ArrayList<>();
+        for (int n : numbers) {
+            if (n < text.size()) {
+                requestedLines.add(text.get(n));
+            }
+        }
+        return requestedLines;
+    }
+}
