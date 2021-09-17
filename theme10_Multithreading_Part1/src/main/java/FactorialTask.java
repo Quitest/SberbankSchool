e69b11380f82cb0e941ef557f50f509c5b81f49d
lenovo <none@none.org>
2021-09-17 14:51:48 +0300

    Theme 10. Первая рабочая версия.

--- theme10_Multithreading_Part1/src/main/java/FactorialTask.java
+++ theme10_Multithreading_Part1/src/main/java/FactorialTask.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class FactorialTask implements Runnable {
-    private String file;
+    private final String file;
 
     public FactorialTask(String file) {
         this.file = file;
@@ -15,27 +16,37 @@ public class FactorialTask implements Runnable {
     @Override
     public void run() {
         int number = takeANumberFromFile();
-        long factorial = 1;
-        while (number > 0) {
-            factorial *= number--;
+        int multiplier = number;
+        BigDecimal factorial = BigDecimal.ONE;
+
+        while (multiplier > 0) {
+            BigDecimal n = new BigDecimal(multiplier--);
+            factorial = factorial.multiply(n);
         }
-        System.out.printf("Thread %s has calculated %d! = %d %n", Thread.currentThread().getName(), number, factorial);
+        System.out.printf("Thread %s has calculated %d! = %s %n", Thread.currentThread().getName(), number, factorial);
     }
 
     private int takeANumberFromFile() {
         int n = 0;
 
-        try {
-            List<String> numbersList = Files.lines(Path.of(file))
-                    .collect(Collectors.toList());
-            n = Integer.parseInt(numbersList.get(numbersList.size() - 2));
-            PrintWriter writer = new PrintWriter(file);
-            for (int i = 0; i < numbersList.size() - 1; i++) {
-                writer.println(numbersList.get(i));
+        synchronized (file) {
+            try {
+                List<String> numbersList = Files.lines(Path.of(file))
+                        .filter(str -> !str.isEmpty())
+                        .collect(Collectors.toList());
+                if (numbersList.size()<=0){
+                    throw new IllegalArgumentException("Нет в файле нет чисел для вычисления факториала");
+                }
+                n = Integer.parseInt(numbersList.get(numbersList.size() - 1));
+                PrintWriter writer = new PrintWriter(file);
+                for (int i = 0; i < numbersList.size() - 1; i++) {
+                    writer.println(numbersList.get(i));
+                    writer.flush();
+                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         return n;
     }
--- theme10_Multithreading_Part1/src/main/java/Main.java
+++ theme10_Multithreading_Part1/src/main/java/Main.java
@@ -3,16 +3,29 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class Main {
+    private static final int THREADS_MAX = 10;
+
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         String fileWithNumbers = "numbers.txt";
-        System.out.println("START" + Thread.currentThread().getName());
+//        System.out.println("START" + Thread.currentThread().getName());
 
         generateFile(fileWithNumbers);
-        Thread t0 = new Thread(new FactorialTask(fileWithNumbers));
-        t0.start();
-        t0.join();
 
-        System.out.println("END"+ Thread.currentThread().getName());
+        for (int i = 0; i < THREADS_MAX; i++){
+            Thread thread = new Thread(new FactorialTask(fileWithNumbers));
+            thread.start();
+            thread.join();
+        }
+
+//            Thread t0 = new Thread(new FactorialTask(fileWithNumbers));
+//        Thread t1 = new Thread(new FactorialTask(fileWithNumbers));
+//
+//        t0.start();
+//        t1.start();
+
+
+
+//        System.out.println("END"+ Thread.currentThread().getName());
     }
 
     public static void generateFile(String fileName) throws FileNotFoundException {
